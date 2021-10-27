0e20c964986c7e3183b01707e0758d184ceb546e
LB197 <predat97@gmail.com>
2021-10-27 19:48:06 +0200

    lab01 done

--- lab01/INCA_libs_gui/run.lnx86.19.09.d/.timestamp.ts
+++
@@ -1,2 +0,0 @@
-1634815019 /home/student/lblach/VDIC/lab01/mtm_Alu_tb.sv
-1634224339 /home/student/lblach/VDIC/lab01/mtm_Alu.vp
--- lab01/mtm_Alu_tb.sv
+++ lab01/mtm_Alu_tb.sv
@@ -10,11 +10,14 @@ module mtm_Alu_tb();
 		add_op                   = 3'b100,
 		sub_op                   = 3'b101
 	} operation_t;
+
+	typedef enum bit {DATA = 1'b0, CTL = 1'b1} packet_type_t;
+
 	bit                 clk;
 	bit                 rst_n;
 	bit                 sin;
 	bit                 sout;
-	operation_t         op_set;
+	operation_t         op_code;
 
 	string             test_result = "PASSED";
 
@@ -41,7 +44,6 @@ module mtm_Alu_tb();
 		end
 	end
 
-
 //------------------------------------------------------------------------------
 // Tester
 //------------------------------------------------------------------------------
@@ -50,17 +52,21 @@ module mtm_Alu_tb();
 // Random data generation functions
 
 	function operation_t get_op();
-		bit [1:0] op_choice;
-		op_choice = $random;
+		bit [2:0] op_choice;
+		op_choice = $random%5;
 		case (op_choice)
-			2'b00 : return and_op;
-			2'b01 : return or_op;
-			2'b10 : return add_op;
-			2'b11 : return sub_op;
+			3'b000 : return and_op;
+			3'b001 : return or_op;
+			3'b010 : return add_op;
+			3'b011 : return sub_op;
+			3'b100 : return operation_t'(3'b010);
+			3'b101 : return operation_t'(3'b011);
+//          3'b110 : return operation_t'(3'b110);
+//          3'b111 : return operation_t'(3'b111);
 		endcase // case (op_choice)
 	endfunction : get_op
-//---------------------------------
 
+//---------------------------------
 	function bit [31:0] get_data();
 		bit [1:0] zero_ones;
 		zero_ones = 2'($random);
@@ -69,86 +75,178 @@ module mtm_Alu_tb();
 		else if (zero_ones == 2'b11)
 			return 32'hFFFFFFFF;
 		else
-			return 32'($urandom%20);
+			return 32'($random);
 	endfunction : get_data
 
 //---------------------------------
 
-	function [43:0] data_packet(input [31:0] A);
-		return {2'b00,A[31:24], 1'b1,2'b00,A[23:16], 1'b1,2'b00,A[15:8], 1'b1,2'b00,A[7:0], 1'b1};
-	endfunction : data_packet
+	task send_packet(packet_type_t packet_type, input bit [7:0] input_8b_data);
+		integer i;
+		begin
+			@(negedge clk) sin = 1'b0;
+			@(negedge clk) sin = packet_type;
+			for (i = 0; i < 8; i = i + 1)
+				@(negedge clk) sin = input_8b_data[7-i];
+			@(negedge clk) sin = 1'b1;
+		end
+	endtask : send_packet
 
 //---------------------------------
 
-	function [98:0] data_frame(input [43:0] A, input [43:0]B, [2:0] op_set, [3:0]CRC);
-		return {B, A, 3'b010,op_set,CRC,1'b1};
+	task send_32b_data_packet(input bit [31:0] input_32b_data, bit data_error);
+		integer i;
+		begin
+			for (i = 0; i < (data_error ? $random%3 : 4); i = i + 1)
+				send_packet(DATA, input_32b_data[31-8*i-:8]);
+		end
+	endtask : send_32b_data_packet
 
-	endfunction : data_frame
-//------------------------
+//---------------------------------
+
+	task send_ctl_packet(input operation_t op_code, input bit [3:0] CRCin);
+		begin
+			send_packet(CTL, {1'b0, op_code, CRCin});
+		end
+	endtask : send_ctl_packet
+
+//---------------------------------
+	task send_data_to_input(input bit [31:0] A, input bit [31:0] B, input operation_t op_code, input bit crc_error, input bit data_nr_error, input bit data_bit_error);
+		integer bit_data_error;
+
+		bit [31:0] A_val, B_val;
+		bit [3:0] CRCin;
+		bit corrupted_bit;
+		corrupted_bit = $random%31;
+
+		A_val = A;
+		B_val = B;
+		if (data_bit_error && ~crc_error) begin
+			if (1'($random)) A_val[corrupted_bit] = ~A_val[corrupted_bit];
+			else B_val[corrupted_bit] = ~B_val[corrupted_bit];
+		end
+
+		begin
+			CRCin = calculate_CRCin(A, B, op_code);
+			send_32b_data_packet(B_val, data_nr_error);
+			send_32b_data_packet(A_val, data_nr_error);
+			if(crc_error) CRCin = CRCin ^ 4'b0001;
+			send_ctl_packet(op_code, CRCin);
+		end
+	endtask : send_data_to_input
+
+	task read_packet (output bit [7:0] output_8b_data, output packet_type_t packet_type);
+		integer i;
+		begin
+			wait(sout == 1'b0);
+			@(negedge clk);
+			@(negedge clk) packet_type = packet_type_t'(sout);
+			for (i = 0; i < 8; i = i + 1)
+				@(negedge clk) output_8b_data[7 - i] = sout;
+			wait(sout == 1'b1);
+		end
+
+	endtask : read_packet
+
+	task read_data_from_output(output [31:0] C, output [7:0] ctl);
+		integer i;
+		bit [7:0] data_out[5];
+		packet_type_t data_type[5];
+
+		begin
+			read_packet(data_out[0], data_type[0]);
+			case (data_type[0])
+				DATA: begin: no_error
+					for (i = 1; i < 5; i = i + 1)
+						read_packet(data_out[i], data_type[i]);
+					C = {data_out[0], data_out[1], data_out[2], data_out[3]};
+					ctl = data_out[4][7:0];
+				end
+				CTL: begin: error
+					ctl = data_out[0][7:0];
+				end
+			endcase
+		end
+	endtask : read_data_from_output
+//---------------------------------
+
 // Tester main
 	initial begin : tester
 		integer i;
-		bit [2:0] op_set;
-		bit [3:0] CRCin, flags_out;
-		bit [2:0] CRCout;
-		bit [31:0] A_data, B_data;
+		integer j;
+		bit [3:0] CRCin, calc_crc,  flags_out, flags_exp;
+		bit [2:0] CRCout, CRCexp, errors;
+		bit [31:0] A_data, B_data, C, Cexp, expected_value;
 		bit [43:0] A_packet, B_packet;
-		bit [98:0] data_in;
+		bit [98:0] data_in, data_prep;
 		bit [54:0] data_out;
-		bit [31:0] expected_value;
-		bit [31:0] C;
+		bit ERR_CRC, ERR_OP, ERR_DATA, ERR_BIT;
+		bit [5:0] error_flags, error_exp;
+		bit parity, parity_exp;
+		bit [7:0] ctl, ctl_exp;
+		bit crc_error;
+		bit [3:0] allzeroone;
+		operation_t op_set;
+
+		i = 0;
+		j = 0;
 
 		reset_alu();
 
+		repeat (100000) begin : tester_main
+			@(negedge clk)
 
-		repeat (20) begin : tester_main
+				//reset before operation
+				if(3'($random) == 0) reset_alu();
+
+			ERR_CRC  =  1'b0;
+			ERR_OP   =  1'b0;
+			ERR_DATA =  1'b0;
+			ERR_BIT  =  1'b0;
 
 			op_set = get_op();
-			A_data = get_data();
-			B_data = get_data();
-			A_packet = data_packet(A_data);
-			B_packet = data_packet(B_data);
-			CRCin = calculate_CRCin(A_data, B_data, op_set);
+				
+			allzeroone = 4'($random);
+			if(allzeroone == 4'h0) begin
+				A_data = 32'h00000000;
+				B_data = 32'h00000000;
+			end else if (allzeroone == 4'hF) begin
+				A_data = 32'hFFFFFFFF;
+				B_data = 32'hFFFFFFFF;
+			end else begin
+				A_data = get_data();
+				B_data = get_data();
+			end
 			
-			data_in = data_frame(A_packet, B_packet, op_set, CRCin);
+			if (2'($random) == 0) ERR_BIT = 1'b1;
+			if ((op_set == 3'b010) || (op_set == 3'b011)) ERR_OP = 1'b1;
+			if (2'($random) == 0) ERR_CRC = 1'b1;
+			if (2'($random) == 0) ERR_DATA = 1'b1;
+			errors = {ERR_DATA, (ERR_CRC | ERR_BIT), ERR_OP};
+			$display("errors %b op_set %b", errors, op_set);
 
-
-
-			$display("Input data A=%d B=%d, OP_code %b, CRC: %b", $signed(A_data), $signed(B_data), op_set, CRCin);
-			
-			//------------------------------------------------------------------------------
-			//Write data to input
-			write_in(data_in);		
-			read_out(C, flags_out, CRCout);
-
-			//------------------------------------------------------------------------------
-			//Read data from output
-
-//			@(negedge sout) begin
-//				foreach (data_out[i]) @(negedge clk) data_out[54 - i] = sout;
-//			end
-			
-			//------------------------------------------------------------------------------
-			// Data processing
-//				temp_data = {data_out[42:35],data_out[31:24],data_out[20:13], data_out[9:2]};
-//				temp_crc = data_out[53:51];
-//				temp_flags = data_out[50:47];
-//				foreach (temp_data[i]) C[i] = temp_data[31 - i];
-//				foreach (temp_crc[i]) CRCout = temp_crc[2 - i];
-//				foreach (temp_flags[i]) flags_out = temp_flags[3 - i];
+			send_data_to_input(A_data, B_data, op_set, ERR_CRC, ERR_DATA, ERR_BIT);
+			$display("Input data A=%d B=%d, OP_set %b", $signed(A_data), $signed(B_data), op_set);
+			read_data_from_output(C, ctl);
 
 			//------------------------------------------------------------------------------
 			// temporary data check - scoreboard will do the job later
-
-			expected_value = get_expected(A_data, B_data, op_set);
-				$display("expected: %b", expected_value);
-			if(C === expected_value) begin
-				$display("Test passed with results C: %d, expected: %d\n", $signed(C), $signed(expected_value));
-			end else begin
-				$display("Test FAILED with results C: %d, expected: %d\n", $signed(C), $signed(expected_value));
+			get_expected(A_data, B_data, op_set, errors, Cexp, ctl_exp);
+			$display("ctl_prs:%b", ctl);
+			$display("ctl_exp:%b", ctl_exp);
+			$display("Cprs:%b", C);
+			$display("Cexp:%b\n", Cexp);
+			begin
+				if ((ctl == ctl_exp) && (C == Cexp) && (errors == 0)) i = i+1;
+				else if ((ctl == ctl_exp) && (errors > 0)) i = i+1;
+				else j = j + 1;
 			end
+
+			//reset after operation
+			if(3'($random) == 0) reset_alu();
 		end
+		$display("passed: %d, failed: %d", i, j);
 		$finish;
+
 	end : tester
 
 //------------------------------------------------------------------------------
@@ -165,64 +263,67 @@ module mtm_Alu_tb();
 	endtask
 
 //------------------------------------------------------------------------------
-// Write data to input -- input_data - data you want to send
-//------------------------------------------------------------------------------
-	task write_in(bit[98:0] input_data);
-	`ifdef DEBUG
-		$display("%0t DEBUG: writing data to input", $time);
-	`endif
-		foreach (input_data[i]) @(negedge clk) sin = input_data[i];
-	endtask
-	
-//------------------------------------------------------------------------------
-// Read data from output -- output_data - returned value with swapped order of the bits
-//------------------------------------------------------------------------------
-	task read_out(output bit [31:0] C, output bit [3:0] flags_out, output bit [2:0] CRCout);		
-		bit [54:0] data_out;
-		bit [31:0] temp_data;
-		bit [3:0] temp_flags;
-		bit [2:0] temp_crc;
-		`ifdef DEBUG
-			$display("%0t DEBUG: reading data from output", $time);
-		`endif
-			@(negedge sout) begin
-				foreach (data_out[i]) @(negedge clk) data_out[54 - i] = sout;
-			end
-			
-			temp_data = {data_out[42:35],data_out[31:24],data_out[20:13], data_out[9:2]};
-			temp_crc = data_out[53:51];
-			temp_flags = data_out[50:47];
-			foreach (temp_data[i]) C[i] = temp_data[31 - i];
-			foreach (temp_crc[i]) CRCout[i] = temp_crc[2 - i];
-			foreach (temp_flags[i]) flags_out[i] = temp_flags[3 - i];
-	endtask
-
-
-//------------------------------------------------------------------------------
 // calculate expected result
 //------------------------------------------------------------------------------
-	function logic [31:0] get_expected(
-			bit [31:0] A,
-			bit [31:0] B,
-			operation_t op_set
+	task get_expected(
+			input       bit [31:0]  A_data, B_data,
+			input       operation_t op_set,
+			input       bit [2:0] errors,
+			output      bit [31:0]  Cexp,
+			output      bit [7:0]   CTL_exp
 		);
-		bit [31:0] ret;
-	`ifdef DEBUG
-		$display("%0t DEBUG: get_expected(%0d,%0d,%0d)",$time, A, B, op_set);
-	`endif
+
+		bit Carry, Overflow, Negative, Zero, parity;
+		bit [32:0] Cext;
+		bit [3:0]   flags_exp;
+		bit [2:0]   CRCexp;
+		bit [5:0]   error_flags;
+
+		Carry =     1'b0;
+		Overflow =  1'b0;
+		Negative =  1'b0;
+		Zero =      1'b0;
+		Cext =      0;
+  `ifdef DEBUG
+		$display("%0t DEBUG: get_expected(%0d,%0d,%0d)",$time, A_data, B_data, op_set);
+  `endif
+
 		case(op_set)
-			and_op : ret = B & A;
-			or_op  : ret = B | A;
-			add_op : ret = B + A;
-			sub_op : ret = B - A;
-			default: begin
-				$display("%0t INTERNAL ERROR. get_expected: unexpected case argument: %s", $time, op_set);
-				test_result = "FAILED";
-				return -1;
+			and_op : Cexp = B_data & A_data;
+			or_op  : Cexp = B_data | A_data;
+			add_op : begin
+				Cexp = B_data + A_data;
+				Cext = {1'b0, B_data} + {1'b0, A_data};
+				Overflow = (~(1'b0 ^ A_data[31] ^ B_data[31]) & (B_data[31] ^ Cext[31]));
+			end
+			sub_op : begin
+				Cexp = B_data - A_data;
+				Cext = {1'b0, B_data} - {1'b0, A_data};
+				Overflow = (~(1'b1 ^ A_data[31] ^ B_data[31]) & (B_data[31] ^ Cext[31]));
 			end
 		endcase
-		return(ret);
-	endfunction
+		//---------------------------------------
+
+		if (errors > 0) begin
+			if      (errors[2] == 1'b1) error_flags = {6'b100100};
+			else if (errors[1] == 1'b1) error_flags = {6'b010010};
+			else if (errors[0] == 1'b1) error_flags = {6'b001001};
+			else error_flags = {6'b000000};
+			parity = (1'b1 ^ error_flags[5] ^ error_flags[4] ^ error_flags[3] ^ error_flags[2] ^ error_flags[1] ^ error_flags[0]);
+		end
+
+		Zero = !(Cexp[31:0]);
+		Negative = Cexp[31];
+		Carry = Cext[32];
+
+		flags_exp = {Carry, Overflow, Zero, Negative};
+		CRCexp = calculate_CRCout(Cexp, flags_exp);
+
+		if (errors) CTL_exp = {1'b1, error_flags, parity};
+		else CTL_exp = {1'b0, flags_exp, CRCexp};
+
+	endtask
+
 //------------------------------------------------------------------------------
 // Temporary. The scoreboard data will be later used.
 	final begin : finish_of_the_test
@@ -276,7 +377,6 @@ module mtm_Alu_tb();
 			calculate_CRCout = CRC;
 		end
 	endfunction
-
 
 
 endmodule
